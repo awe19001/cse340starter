a94ce0dccd1370f2e0a8a5366e3dbc1d78c8bec4
joeverlyn <joeverlyncapute@gmail.com>
2024-03-14 09:55:12 +0800

    middleware

--- utilities/index.js
+++ utilities/index.js
@@ -1,7 +1,7 @@
 const invModel = require("../models/inventory-model")
 const Util = {}
 
-//console.log(data)
+
 
 /* ************************
  * Constructs the nav HTML unordered list
@@ -61,5 +61,15 @@ Util.buildClassificationGrid = async function(data){
   }
   return grid
 }
+
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+
 
 module.exports = Util
