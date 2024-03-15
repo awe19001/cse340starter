0fe66cac3089a2a48fad16ee10940d21180e63a5
joeverlyn <joeverlyncapute@gmail.com>
2024-03-16 05:04:36 +0800

    ambot

--- server.js
+++ server.js
@@ -11,18 +11,48 @@ const env = require("dotenv").config()
 const app = express()
 const baseController = require("./controllers/baseController")
 const utilities = require("./utilities/")
-const pool = require("./database")
-
-
-
+const session = require("express-session")
+const pool = require('./database/')
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
 /* ***********************
- * View Engine and Templates
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+// Use body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Use cookie parser
+app.use(cookieParser())
+
+// Use token validation
+app.use(utilities.checkJWTToken)
+
+/* ***********************
+ * View Engine and Template
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
@@ -30,17 +60,25 @@ app.set("layout", "./layouts/layout") // not at views root
 // static route
 app.use(require("./routes/static"))
 
-// Index route
+// index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
 
-// Inventory routes
+// inventory route
 app.use("/inv", require("./routes/inventoryRoute"))
+
+// account route
+app.use("/account", require("./routes/accountRoute"))
+
+// inbox route
+app.use("/inbox", require("./routes/messageRoute"))
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+  next({
+    status: 404, 
+    message: 'This is not the page you are looking for... Go about your business... Move along...'
+  })
 })
-
 
 /* ***********************
 * Express Error Handler
@@ -49,11 +87,11 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  if(err.status == 404) {message = err.message} else {message = 'Oops, looks like something went wrong! Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
-    nav
+    nav,
   })
 })
 
